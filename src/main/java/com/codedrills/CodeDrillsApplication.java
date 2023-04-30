9000228ae2ac7a2f55ca9526e7d15359e632dd89
Ankur Kayal <ankurkayal1234@gmail.com>
2023-04-30 15:11:24 +0530

    Configured RestTemplate for https links (#43)

--- src/main/java/com/codedrills/CodeDrillsApplication.java
+++ src/main/java/com/codedrills/CodeDrillsApplication.java
@@ -1,8 +1,12 @@
 package com.codedrills;
 
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,7 +16,12 @@ public class CodeDrillsApplication {
 
   @Bean
   public RestTemplate restTemplate() {
-    return new RestTemplate();
+    CloseableHttpClient httpClient = HttpClients.custom()
+            .setSSLHostnameVerifier(new NoopHostnameVerifier())
+            .build();
+    HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+    requestFactory.setHttpClient(httpClient);
+    return new RestTemplate(requestFactory);
   }
 
   public static void main(String[] args) {
